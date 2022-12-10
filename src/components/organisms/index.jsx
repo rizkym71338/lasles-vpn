8009ccade11b33a6fa5b3f4bfb5a6029d9e9fd67
Rizky Maulana <rizkym71338@gmail.com>
2022-12-10 17:43:24 +0700

    .

--- src/components/organisms/index.jsx
+++ src/components/organisms/index.jsx
@@ -4,3 +4,4 @@ export { default as Hero } from "./Hero";
 export { default as Feature } from "./Feature";
 export { default as Plan } from "./Plan";
 export { default as Network } from "./Network";
+export { default as Testimonial } from "./Testimonial";
